f42fa3c1c99f90e8c516653a6e69f7a1e47bfb80
julio gabriel <juliogabriel78956@gmail.com>
2025-02-22 19:21:26 -0300

    tarefa

---
+++ app.js
@@ -0,0 +1,176 @@
+//let frutas = ["Maça", "Manga","banana","laranja","Maracuja","Sapoti","Morango",]
+//console.log(`minha furta preferida e ${frutas[4]} `)
+
+//console.log(`o artista que eu mais escuto e o ${artistas[2]}`)
+
+//console.log(` o artista que eu detest e ${artistas[1]}`)
+
+//let numeros = [1,2,3,4,5]
+
+//let soma = numeros [0] + numeros [4]
+
+//let exemplo = [1,3,["Teste","Desse","Array"],[true,2,"Undefined"],5,["Pera","Uva","Maça"],[2,4,6,3]]
+
+//console.log(exemplo [2] [0])
+
+//console.log(exemplo [3][2] ) //ele e tipo texto string pois esta entre aspas
+
+//console.log(exemplo [5][2])
+
+//console.log(exemplo [6][2])
+
+//console.log(exemplo [2][0])
+
+//let carro = {
+
+    //id:1,
+    //nome:"chevette",
+    //ano: 1979,
+    //cor:"black piano",
+    //valor:10000
+//}
+//console.log(`o carro ${carro.nome}de ano ${carro.ano} e cor ${carro.cor} esta custando apenas ${carro.valor} reais! `)
+
+//let carros = [
+   // { 
+     //   id: 1,
+      //  modelo: "Fusca",
+      //  ano: 1979,
+      //  cor: "Amarela"
+  //  },
+   // { 
+     //   id: 2,
+     //   modelo: "Gol",
+      //  ano: 1990,
+      //  cor: "Azul"
+   // },
+   // { 
+      //  id: 3,
+      //  modelo: "Civic",
+       //ano: 2005,
+      //  cor: "Preta"
+    //},
+    //{ 
+      //  id: 4,
+      //  modelo: "Palio",
+      ///  ano: 2002,
+       // cor: "Vermelha"
+    //},
+   // { 
+      // // id: 5,
+      //  modelo: "Fiesta",
+       // ano: 2010,
+       // cor: "Branca"
+    //},
+    //{ 
+     //   id: 6,
+      //  modelo: "Kangoo",
+      //  ano: 2015,
+       // cor: "Prata"
+    //},
+    //{ 
+     //   id: 7,
+      //  modelo: "Uno",
+      //  ano: 1995,
+       // cor: "Verde"
+    //},
+   // { 
+       // id: 8,
+      //  modelo: "Saveiro",
+      //  ano: 2012,
+      //  cor: "Preta"
+    //},
+    //{ 
+      //  id: 9,
+      ///  modelo: "Strada",
+      //  ano: 2017,
+      //  cor: "Branca"
+    //},
+    //{ 
+      //  id: 10,
+      //  modelo: "Siena",
+      //  ano: 2008,
+      //  cor: "Cinza"
+    //}
+//];
+// console.log(`meu carro ${carros[2].modelo} julio você acabou comoprando um carro 0 km`)
+
+//console.log(`carro Fiesta de cor ${carros[4].cor} 0km pronto para ser comprado`)
+
+//console.log(`o carro ${carros[6].modelo} do ano ${carros[6].ano}esta pronta para ser vendida`)
+
+//console.log(`o carro de modelo ${carros[2].modelo} tem a cor ${carros[2].cor} e foi frabricado no ano de ${carros[2].ano}`)
+
+//console.log(`o ${carros[9].modelo} de cor cinza foi fabricado no ano de ${carros[9].ano} `)
+
+//console.log(`o carrro do modelo ${carros[4].modelo}do ano de ${carros[4].cor } foi fabricado em 2010`)
+
+let filmes = [
+    { 
+        id: 1,
+        titulo: "O Senhor dos Anéis: A Sociedade do Anel",
+        ano: 2001,
+        genero: "Fantasia"
+    },
+    { 
+        id: 2,
+        titulo: "O Poderoso Chefão",
+        ano: 1972,
+        genero: "Crime"
+    },
+    { 
+        id: 3,
+        titulo: "Forrest Gump: O Contador de Histórias",
+        ano: 1994,
+        genero: "Drama"
+    },
+    { 
+        id: 4,
+        titulo: "Star Wars: O Império Contra-Ataca",
+        ano: 1980,
+        genero: "Ficção científica"
+    },
+    { 
+        id: 5,
+        titulo: "A Origem",
+        ano: 2010,
+        genero: "Ficção científica"
+    },
+    { 
+        id: 6,
+        titulo: "O Rei Leão",
+        ano: 1994,
+        genero: "Animação"
+    },
+    { 
+        id: 7,
+        titulo: "Titanic",
+        ano: 1997,
+        genero: "Romance"
+    },
+    { 
+        id: 8,
+        titulo: "Vingadores: Ultimato",
+        ano: 2019,
+        genero: "Ação"
+    },
+    { 
+        id: 9,
+        titulo: "O Exorcista",
+        ano: 1973,
+        genero: "Terror"
+    },
+    { 
+        id: 10,
+        titulo: "Jurassic Park",
+        ano: 1993,
+        genero: "Aventura"
+    }	
+]; 
+//console.log(`O filme ${filmes[3].titulo} tem seu genero de ficção cientifica`)
+
+//console.log(`o filme ${filmes[6].titulo} e famoso pela sua grande arrecadação de dinheiro e seu genero de filme e romance`)
+
+//console.log(`o filme titanic foi lançado em ${filmes[6].ano}`)
+
+//console.log(`o filme ${filmes[5].titulo} conhecido por todos quem não conhece não teve infancia mas para quem não sabe o filme foi lançado em ${filmes[5].ano} `)
